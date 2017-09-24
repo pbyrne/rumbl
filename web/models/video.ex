6e1654cb6577fb99f566ae6f81f010a18f87af05
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2017-09-24 15:10:09 -0500

    Videos belong in categories

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,16 +6,19 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User, foreign_key: :user_id
+    belongs_to :category, Rumbl.Category, foreign_key: :category_id
 
     timestamps()
   end
+
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
-    |> validate_required([:url, :title, :description])
+    |> cast(params, @required_fields, @optional_fields)
   end
 end
